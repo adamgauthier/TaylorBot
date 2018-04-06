41b676a30f344b0d3d1d3fcf394f72b371ee4125
Adam Gauthier <adamgauthier12@gmail.com>
2018-04-05 23:08:21 -0400

    refactor for GlobalPaths

--- src/tools/GlobalPaths.js
+++ src/tools/GlobalPaths.js
@@ -22,6 +22,67 @@ class GlobalPaths {
                     'MyApiFilmsConfig': 'myapifilms.json',
                     'PostgreSQLConfig': 'postgresql.json'
                 }
+            },
+            {
+                'directory': pathMapper.database.path,
+                'files': {
+                    'DatabaseDriver': 'DatabaseDriver'
+                }
+            },
+            {
+                'directory': pathMapper.structures.path,
+                'files': {
+                    'EventHandler': 'EventHandler',
+                    'Interval': 'Interval',
+                    'MessageWatcher': 'MessageWatcher',
+                    'Command': 'Command',
+                    'ArgumentType': 'ArgumentType',
+                    'Inhibitor': 'Inhibitor'
+                }
+            },
+            {
+                'directory': pathMapper.client.path,
+                'files': {
+                    'UserGroups': 'UserGroups.json',
+                    'GuildRegistry': 'GuildRegistry',
+                    'UserRegistry': 'UserRegistry',
+                    'CommandRegistry': 'CommandRegistry',
+                    'TypeRegistry': 'TypeRegistry',
+                    'GroupRegistry': 'GroupRegistry',
+                    'GuildRoleGroupRegistry': 'GuildRoleGroupRegistry',
+                    'Registry': 'Registry',
+                    'taylorBotClient': 'TaylorBotClient'
+                }
+            },
+            {
+                'directory': pathMapper.tools.path,
+                'files': {
+                    'Logger': 'Logger'
+                }
+            },
+            {
+                'directory': pathMapper.modules.path,
+                'files': {
+                    'StringUtil': 'StringUtil',
+                    'TimeUtil': 'TimeUtil',
+                    'DiscordFormatter': 'DiscordFormatter',
+                    'DiscordEmbedFormatter': 'DiscordEmbedFormatter',
+                    'EventLoader': 'EventLoader',
+                    'InstagramModule': 'InstagramModule',
+                    'TumblrModule': 'TumblrModule',
+                    'RedditModule': 'RedditModule',
+                    'YoutubeModule': 'YoutubeModule',
+                    'CommandLoader': 'CommandLoader',
+                    'TypeLoader': 'TypeLoader',
+                    'IntervalRunner': 'IntervalRunner',
+                    'MessageWatcherRegistry': 'MessageWatcherRegistry'
+                }
+            },
+            {
+                'directory': pathMapper.constants.path,
+                'files': {
+                    'ArgumentInfos': 'ArgumentInfos'
+                }
             }
         ];
 
@@ -30,41 +91,6 @@ class GlobalPaths {
                 this[propertyName] = path.join(mappedPath.directory, mappedPath.files[propertyName]);
             }
         }
-
-
-        this.DatabaseDriver = path.join(pathMapper.database.path, 'DatabaseDriver');
-        this.EventHandler = path.join(pathMapper.structures.path, 'EventHandler');
-        this.Interval = path.join(pathMapper.structures.path, 'Interval');
-        this.MessageWatcher = path.join(pathMapper.structures.path, 'MessageWatcher');
-        this.Command = path.join(pathMapper.structures.path, 'Command');
-        this.ArgumentInfo = path.join(pathMapper.structures.path, 'ArgumentInfo');
-        this.ArgumentType = path.join(pathMapper.structures.path, 'ArgumentType');
-        this.UserGroups = path.join(pathMapper.client.path, 'UserGroups.json');
-        this.StringUtil = path.join(pathMapper.modules.path, 'StringUtil');
-        this.TimeUtil = path.join(pathMapper.modules.path, 'TimeUtil');
-        this.DiscordFormatter = path.join(pathMapper.modules.path, 'DiscordFormatter');
-        this.DiscordEmbedFormatter = path.join(pathMapper.modules.path, 'DiscordEmbedFormatter');
-        this.Logger = path.join(pathMapper.tools.path, 'Logger');
-        this.TumblrModule = path.join(pathMapper.modules.path, 'TumblrModule');
-        this.InstagramModule = path.join(pathMapper.modules.path, 'InstagramModule');
-        this.RedditModule = path.join(pathMapper.modules.path, 'RedditModule');
-        this.YoutubeModule = path.join(pathMapper.modules.path, 'YoutubeModule');
-        this.GuildRegistry = path.join(pathMapper.client.path, 'GuildRegistry');
-        this.UserRegistry = path.join(pathMapper.client.path, 'UserRegistry');
-        this.CommandRegistry = path.join(pathMapper.client.path, 'CommandRegistry');
-        this.TypeRegistry = path.join(pathMapper.client.path, 'TypeRegistry');
-        this.GroupRegistry = path.join(pathMapper.client.path, 'GroupRegistry');
-        this.GuildRoleGroupRegistry = path.join(pathMapper.client.path, 'GuildRoleGroupRegistry');
-        this.Registry = path.join(pathMapper.client.path, 'Registry');
-        this.EventLoader = path.join(pathMapper.modules.path, 'EventLoader');
-        this.CommandLoader = path.join(pathMapper.modules.path, 'CommandLoader');
-        this.TypeLoader = path.join(pathMapper.modules.path, 'TypeLoader');
-        this.IntervalRunner = path.join(pathMapper.modules.path, 'IntervalRunner');
-        this.MessageWatcherRegistry = path.join(pathMapper.modules.path, 'MessageWatcherRegistry');
-        this.ArgumentInfos = path.join(pathMapper.constants.path, 'ArgumentInfos');
-        this.Inhibitor = path.join(pathMapper.structures.path, 'Inhibitor');
-
-        this.taylorBotClient = path.join(pathMapper.client.path, 'TaylorBotClient');
 
         this.eventsFolderPath = pathMapper.events.path;
         this.intervalsFolderPath = pathMapper.intervals.path;
