d21342fedc0e57564445287bc5ad46d2508bb159
Adam Gauthier <adamgauthier12@gmail.com>
2018-05-27 13:40:44 -0400

    split ignored and cooldown inhibitors

--- src/inhibitors/CooldownInhibitor.js
+++ src/inhibitors/CooldownInhibitor.js
@@ -5,19 +5,11 @@ const { Paths } = require('globalobjects');
 const Inhibitor = require(Paths.Inhibitor);
 const Log = require(Paths.Logger);
 const Format = require(Paths.DiscordFormatter);
-const TimeUtil = require(Paths.TimeUtil);
 
 class CooldownInhibitor extends Inhibitor {
     shouldBeBlocked({ message, client }, command) {
         const { author } = message;
-
-        const commandTime = new Date().getTime();
-        const { lastCommand, lastAnswered, ignoreUntil } = client.master.registry.users.get(author.id);
-
-        if (commandTime < ignoreUntil) {
-            Log.verbose(`Command '${command.name}' can't be used by ${Format.user(author)} because they are ignored until ${TimeUtil.formatLog(ignoreUntil)}.`);
-            return true;
-        }
+        const { lastCommand, lastAnswered } = client.master.registry.users.get(author.id);
 
         if (lastAnswered < lastCommand) {
             Log.verbose(`Command '${command.name}' can't be used by ${Format.user(author)} because they have not been answered. LastAnswered:${lastAnswered}, LastCommand:${lastCommand}.`);
---
+++ src/inhibitors/IgnoredInhibitor.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { Paths } = require('globalobjects');
+
+const Inhibitor = require(Paths.Inhibitor);
+const Log = require(Paths.Logger);
+const Format = require(Paths.DiscordFormatter);
+const TimeUtil = require(Paths.TimeUtil);
+
+class IgnoredInhibitor extends Inhibitor {
+    shouldBeBlocked({ message, client }, command) {
+        const { author } = message;
+
+        const commandTime = new Date().getTime();
+        const { ignoreUntil } = client.master.registry.users.get(author.id);
+
+        if (commandTime < ignoreUntil) {
+            Log.verbose(`Command '${command.name}' can't be used by ${Format.user(author)} because they are ignored until ${TimeUtil.formatLog(ignoreUntil)}.`);
+            return true;
+        }
+
+        return false;
+    }
+}
+
+module.exports = IgnoredInhibitor;
