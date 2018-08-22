35433e0c5ff8fd2db959e1db821479311470fdc2
Adam Gauthier <adamgauthier12@gmail.com>
2018-08-22 18:04:24 -0400

    add Imgur command

---
+++ src/commands/imgur/Imgur.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Command = require('../../structures/Command.js');
+const CommandError = require('../../structures/CommandError.js');
+const Imgur = require('../../modules/imgur/ImgurModule.js');
+const DiscordEmbedFormatter = require('../../modules/DiscordEmbedFormatter.js');
+
+class ImgurCommand extends Command {
+    constructor() {
+        super({
+            name: 'imgur',
+            group: 'imgur',
+            description: 'Upload a picture on Imgur!',
+            examples: ['imgur '],
+
+            args: [
+                {
+                    key: 'url',
+                    label: 'url',
+                    type: 'url',
+                    prompt: `What's the link to the picture you would you like upload?`
+                }
+            ]
+        });
+    }
+
+    async run({ message, client }, { url }) {
+        const { author, channel } = message;
+
+        const response = await Imgur.upload(url);
+
+        if (!response.success)
+            throw new CommandError(`Something went wrong when uploading to Imgur. 😕`);
+
+        const { link } = response.data;
+
+        return client.sendEmbed(channel, DiscordEmbedFormatter
+            .baseUserEmbed(author)
+            .setDescription(`Successfully uploaded your image to Imgur, it can be found here: ${link} 😊.`)
+            .setImage(link)
+        );
+    }
+}
+
+module.exports = ImgurCommand;
---
+++ src/types/UrlArgumentType.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { URL } = require('url');
+
+const WordArgumentType = require('./WordArgumentType.js');
+const ArgumentParsingError = require('../structures/ArgumentParsingError.js');
+
+class UrlArgumentType extends WordArgumentType {
+    get id() {
+        return 'url';
+    }
+
+    parse(val) {
+        try {
+            return new URL(val);
+        }
+        catch (e) {
+            if (e instanceof TypeError) {
+                throw new ArgumentParsingError(`Could not parse a valid URL from '${val}'`);
+            }
+            else {
+                throw e;
+            }
+        }
+    }
+}
+
+module.exports = UrlArgumentType;
