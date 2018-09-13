b2faa0a3f91fe1f7d861b2de8a6cc3ac3156ef27
Adam Gauthier <adamgauthier12@gmail.com>
2018-09-12 23:35:31 -0400

    add eval time to eval command

--- src/commands/util/Eval.js
+++ src/commands/util/Eval.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const EmbedUtil = require('../../modules/EmbedUtil.js');
 const Command = require('../../structures/Command.js');
 const CommandError = require('../../structures/CommandError.js');
 const UserGroups = require('../../client/UserGroups.json');
@@ -29,14 +30,21 @@ class EvalCommand extends Command {
         const { message, client } = commandMessage;
 
         let result;
+
+        const start = process.hrtime.bigint();
         try {
             result = eval(code);
         } catch (e) {
             Log.error(`Eval error: ${e.stack}`);
             throw new CommandError(e.toString());
         }
+        const end = process.hrtime.bigint();
 
-        return client.sendEmbedSuccess(message.channel, result);
+        return client.sendEmbed(message.channel,
+            EmbedUtil
+                .success(result)
+                .setFooter(`Evaluation took ${end - start} nanoseconds`)
+        );
     }
 }
 
