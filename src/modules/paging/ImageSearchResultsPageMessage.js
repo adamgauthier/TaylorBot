44bf9caa3bf9dd2649093f6996f875cdb0214caa
Adam Gauthier <adamgauthier12@gmail.com>
2018-09-23 14:28:26 -0400

    display google thumbnail when image link is of unknown protocol

--- src/modules/paging/ImageSearchResultsPageMessage.js
+++ src/modules/paging/ImageSearchResultsPageMessage.js
@@ -1,15 +1,20 @@
 'use strict';
 
+const { URL } = require('url');
 const ArrayPageEmbedMessage = require('./ArrayPageEmbedMessage.js');
 
 class ImageSearchResultsPageMessage extends ArrayPageEmbedMessage {
     update() {
         const imageResult = this.pages[this.currentPage];
 
+        const imageURL =
+        ['http:', 'https:'].includes(new URL(imageResult.link).protocol) ?
+            imageResult.link : imageResult.image.thumbnailLink;
+
         this.embed
             .setTitle(imageResult.title)
             .setURL(imageResult.image.contextLink)
-            .setImage(imageResult.link);
+            .setImage(imageURL);
     }
 }
 
