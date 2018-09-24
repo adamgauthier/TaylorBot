e2b9d6216c0fd10ab2ed4ca6a39d49f2fcee20d9
Adam Gauthier <adamgauthier12@gmail.com>
2018-09-24 14:16:01 -0400

    change minutes for reward from 8 to 6

--- src/intervals/MinutesInterval.js
+++ src/intervals/MinutesInterval.js
@@ -5,7 +5,7 @@ const Interval = require('../structures/Interval.js');
 const minutesToAdd = 1;
 const msBeforeAdd = 1 * 60 * 1000;
 const pointsReward = 1;
-const minutesForReward = 8;
+const minutesForReward = 6;
 const msBeforeInactive = 10 * 60 * 1000;
 
 class MinutesInterval extends Interval {
