8c6db3b12b460276afcdda2c28c67c6b908275bf
Adam Gauthier <adamgauthier12@gmail.com>
2018-10-26 01:09:05 -0400

    change getLatestUsernames into pg-promise statement

--- src/database/repositories/UsernameRepository.js
+++ src/database/repositories/UsernameRepository.js
@@ -10,7 +10,15 @@ class UsernameRepository {
 
     async getAllLatest() {
         try {
-            return await this._db.usernames.getLatestUsernames();
+            return await this._db.instance.any(
+                `SELECT u.username, u.user_id
+                FROM (
+                    SELECT user_id, MAX(changed_at) AS max_changed_at
+                    FROM users.usernames
+                    GROUP BY user_id
+                ) AS maxed
+                JOIN users.usernames AS u ON u.user_id = maxed.user_id AND u.changed_at = maxed.max_changed_at;`
+            );
         }
         catch (e) {
             Log.error(`Getting all usernames: ${e}`);
