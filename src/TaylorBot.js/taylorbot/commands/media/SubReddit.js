c33b7fd06be159b3b7c45fe2fdd72840373a1d53
Adam Gauthier <adamgauthier12@gmail.com>
2019-02-21 00:31:40 -0500

    restrict SubReddit to 100 uses per day

--- src/TaylorBot.js/taylorbot/commands/media/SubReddit.js
+++ src/TaylorBot.js/taylorbot/commands/media/SubReddit.js
@@ -14,6 +14,7 @@ class SubRedditCommand extends Command {
             group: 'media',
             description: 'Gets info on a subreddit!',
             examples: ['taylorswift'],
+            maxDailyUseCount: 100,
 
             args: [
                 {
