57ef95646bd1a4f3b39194b05b23aeb3573df908
Adam Gauthier <adamgauthier12@gmail.com>
2019-06-26 20:55:31 -0400

    add all projects at once in manage script

--- src/TaylorBot.Net/manage-nuget-for-all-projects.ps1
+++ src/TaylorBot.Net/manage-nuget-for-all-projects.ps1
@@ -2,9 +2,9 @@ $solutionName = "TaylorBot.Net";
 dotnet new sln --name $solutionName;
 
 $solutionPath = "$solutionName.sln";
-foreach ($project in (Get-ChildItem . -Filter *.csproj -Recurse)) {
-    dotnet sln "$solutionName.sln" add $project.FullName;
-}
+
+$allProjectPaths = (Get-ChildItem . -Filter *.csproj -Recurse) | Select-Object -ExpandProperty FullName;
+dotnet sln "$solutionName.sln" add @allProjectPaths;
 
 (Start-Process "$solutionName.sln" -PassThru).WaitForExit();
 Remove-Item -Path $solutionPath;
