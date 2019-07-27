fee108a2e2e02c90ca8a99f45247133625276144
Adam Gauthier <adamgauthier12@gmail.com>
2019-07-27 18:03:47 -0400

    don't copy .sln in docker files

--- deployment/dotnet/stats-tracker/deploy-stats-tracker.sh
+++ deployment/dotnet/stats-tracker/deploy-stats-tracker.sh
@@ -9,9 +9,9 @@ container_name=$(cat "${__dir}/stats-tracker.name")
 image_name=${container_name}
 network_name=$(cat "${__dir}/../../docker-network.name")
 env_file_path=${__dir}/../taylorbot.net.env
-taylorbot_net_path=${__dir}/../../../src/TaylorBot.Net
+taylorbot_net_path=${__dir}/../../../src/TaylorBot.Net/src
 build_context_path=${taylorbot_net_path}
-docker_file_path=${taylorbot_net_path}/src/TaylorBot.Net.StatsTracker.Program/Dockerfile
+docker_file_path=${taylorbot_net_path}/TaylorBot.Net.StatsTracker.Program/Dockerfile
 
 docker build -t ${image_name} -f ${docker_file_path} ${build_context_path}
 docker container run -d --name ${container_name} --network ${network_name} --env-file ${env_file_path} --restart=on-failure:100 ${image_name}
