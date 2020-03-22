f17726af5b43128f030581721082609b7685cbde
Adam Gauthier <adamgauthier12@gmail.com>
2020-03-22 16:15:21 -0400

    reorganize folder structure

--- deployment/node/taylorbot/deploy-taylorbot-node.sh
+++ deployment/node/taylorbot/deploy-taylorbot-node.sh
@@ -8,7 +8,7 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 container_name=$(cat "${__dir}/taylorbot-node.name")
 image_name=${container_name}
 network_name=$(cat "${__dir}/../../docker-network.name")
-taylorbot_node_path=${__dir}/../../../src/TaylorBot.js/taylorbot
+taylorbot_node_path=${__dir}/../../../src/TaylorBot.js
 build_context_path=${taylorbot_node_path}
 docker_file_path=${taylorbot_node_path}/Dockerfile
 
