8afc9a81d7e41261882fd653a01442e194c393bc
Adam Gauthier <adamgauthier12@gmail.com>
2020-05-13 21:41:31 -0400

    rework TaylorBot.Net folder structure

--- src/TaylorBot.Net/manage-nuget-for-all-projects.ps1
+++ src/TaylorBot.Net/manage-nuget-for-all-projects.ps1
@@ -6,5 +6,5 @@ $solutionPath = "$PSScriptRoot/$solutionName.sln";
 $allProjectPaths = (Get-ChildItem $PSScriptRoot -Filter *.csproj -Recurse) | Select-Object -ExpandProperty FullName;
 dotnet sln $solutionPath add @allProjectPaths;
 
-(Start-Process $solutionPath -PassThru).WaitForExit();
+Start-Process $solutionPath -Wait;
 Remove-Item -Path $solutionPath;
