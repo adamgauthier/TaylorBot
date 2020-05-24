7a20517767d294a5e4dc3401b5e3e99358d54d1b
Adam Gauthier <adamgauthier12@gmail.com>
2020-05-24 12:37:17 -0400

    rework build cycle with Azure Pipelines

---
+++ deploy/dotnet/stats-tracker/deploy-stats-tracker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+network_name=$(cat "${__dir}/../../docker-network.name")
+env_file_path=${__dir}/stats-tracker.env
+image_path=${__dir}/stats-tracker.tar
+container_name=taylorbot-stats-tracker
+
+load_ouput=$(docker load --input ${image_path})
+image_name=${load_ouput##* }
+
+docker container run \
+    --detach \
+    --name ${container_name} \
+    --network ${network_name} \
+    --env-file ${env_file_path} \
+    --restart=on-failure:100 \
+    ${image_name}
+
+docker logs --follow ${container_name}
