0a212d6a36c98be904dc1db3aeba01fa27596740
Adam Gauthier <adamgauthier12@gmail.com>
2020-07-17 00:07:09 -0400

    clear ongoing command after command reply

--- src/TaylorBot.Net/Core/src/TaylorBot.Net.Commands/CommandExecutedHandler.cs
+++ src/TaylorBot.Net/Core/src/TaylorBot.Net.Commands/CommandExecutedHandler.cs
@@ -24,87 +24,86 @@ namespace TaylorBot.Net.Commands
 
         public async Task OnCommandExecutedAsync(Optional<CommandInfo> optCommandInfo, ICommandContext context, IResult result)
         {
-            var commandContext = (TaylorBotShardedCommandContext)context;
+            var commandContext = (ITaylorBotCommandContext)context;
+
+            if (result.Error != CommandError.UnknownCommand)
+            {
+                _logger.LogInformation(LogString.From(
+                    $"{context.User.FormatLog()} used '{context.Message.Content.Replace("\n", "\\n")}' in {context.Channel.FormatLog()}"
+                ));
+
+                if (result.IsSuccess)
+                {
+                    switch (result)
+                    {
+                        case TaylorBotEmbedResult embedResult:
+                            await context.Channel.SendMessageAsync(embed: embedResult.Embed);
+                            break;
+
+                        case TaylorBotEmptyResult emptyResult:
+                            break;
+
+                        default:
+                            throw new InvalidOperationException($"Unexpected command success result: {result.GetType()}");
+                    }
+                    _commandUsageRepository.AddSuccessfulUseCount(optCommandInfo.Value);
+                }
+                else
+                {
+                    switch (result)
+                    {
+                        case ExecuteResult executeResult:
+                            switch (result.Error)
+                            {
+                                case CommandError.Exception:
+                                    _logger.LogError(executeResult.Exception, LogString.From("Unhandled exception in command:"));
+                                    break;
+
+                                default:
+                                    _logger.LogError(executeResult.Exception, LogString.From($"Unhandled error in command - {result.Error}, {result.ErrorReason}:"));
+                                    break;
+                            }
+                            await context.Channel.SendMessageAsync(embed: CreateUnknownErrorEmbed(context.User));
+                            _commandUsageRepository.AddUnhandledErrorCount(optCommandInfo.Value);
+                            break;
+
+                        case ParseResult parseResult:
+                            var cmd = optCommandInfo.Value;
+                            await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                                .WithColor(TaylorBotColors.ErrorColor)
+                                .WithDescription(string.Join('\n',
+                                    $"{context.User.Mention} Format: `{commandContext.GetUsage(cmd)}`",
+                                    parseResult.ErrorParameter != null ?
+                                        $"`<{parseResult.ErrorParameter.Name}>`: {parseResult.ErrorReason}" :
+                                        parseResult.ErrorReason
+                                ))
+                            .Build());
+                            break;
+
+                        case TaylorBotPreconditionResult preconditionResult:
+                            _logger.LogInformation(LogString.From(
+                                $"{commandContext.User.FormatLog()} precondition failure: {preconditionResult.ErrorReason}."
+                            ));
+                            if (preconditionResult.UserReason != null)
+                            {
+                                await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                                    .WithColor(TaylorBotColors.ErrorColor)
+                                    .WithDescription($"{context.User.Mention} {preconditionResult.UserReason}")
+                                .Build());
+                            }
+                            break;
+
+                        default:
+                            _logger.LogError(LogString.From($"Unhandled error in command - {result.Error}, {result.ErrorReason}"));
+                            await context.Channel.SendMessageAsync(embed: CreateUnknownErrorEmbed(context.User));
+                            break;
+                    }
+                }
+            }
 
             if (commandContext.OnGoingCommandAddedToPool != null)
             {
                 await _ongoingCommandRepository.RemoveOngoingCommandAsync(context.User, commandContext.OnGoingCommandAddedToPool);
-            }
-
-            if (result.Error == CommandError.UnknownCommand)
-            {
-                return;
-            }
-
-            _logger.LogInformation(LogString.From(
-                $"{context.User.FormatLog()} used '{context.Message.Content.Replace("\n", "\\n")}' in {context.Channel.FormatLog()}"
-            ));
-
-            if (result.IsSuccess)
-            {
-                switch (result)
-                {
-                    case TaylorBotEmbedResult embedResult:
-                        await context.Channel.SendMessageAsync(embed: embedResult.Embed);
-                        break;
-
-                    case TaylorBotEmptyResult emptyResult:
-                        break;
-
-                    default:
-                        throw new InvalidOperationException($"Unexpected command success result: {result.GetType()}");
-                }
-                _commandUsageRepository.AddSuccessfulUseCount(optCommandInfo.Value);
-                return;
-            }
-
-            switch (result)
-            {
-                case ExecuteResult executeResult:
-                    switch (result.Error)
-                    {
-                        case CommandError.Exception:
-                            _logger.LogError(executeResult.Exception, LogString.From("Unhandled exception in command:"));
-                            break;
-
-                        default:
-                            _logger.LogError(executeResult.Exception, LogString.From($"Unhandled error in command - {result.Error}, {result.ErrorReason}:"));
-                            break;
-                    }
-                    await context.Channel.SendMessageAsync(embed: CreateUnknownErrorEmbed(context.User));
-                    _commandUsageRepository.AddUnhandledErrorCount(optCommandInfo.Value);
-                    break;
-
-                case ParseResult parseResult:
-                    var cmd = optCommandInfo.Value;
-                    await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                        .WithColor(TaylorBotColors.ErrorColor)
-                        .WithDescription(string.Join('\n',
-                            $"{context.User.Mention} Format: `{commandContext.GetUsage(cmd)}`",
-                            parseResult.ErrorParameter != null ?
-                                $"`<{parseResult.ErrorParameter.Name}>`: {parseResult.ErrorReason}" :
-                                parseResult.ErrorReason
-                        ))
-                    .Build());
-                    break;
-
-                case TaylorBotPreconditionResult preconditionResult:
-                    _logger.LogInformation(LogString.From(
-                        $"{commandContext.User.FormatLog()} precondition failure: {preconditionResult.ErrorReason}."
-                    ));
-                    if (preconditionResult.UserReason != null)
-                    {
-                        await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                            .WithColor(TaylorBotColors.ErrorColor)
-                            .WithDescription($"{context.User.Mention} {preconditionResult.UserReason}")
-                        .Build());
-                    }
-                    break;
-
-                default:
-                    _logger.LogError(LogString.From($"Unhandled error in command - {result.Error}, {result.ErrorReason}"));
-                    await context.Channel.SendMessageAsync(embed: CreateUnknownErrorEmbed(context.User));
-                    break;
             }
         }
 
---
+++ src/TaylorBot.Net/Core/test/TaylorBot.Net.Commands.Tests/CommandExecutedHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.Commands;
+using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using TaylorBot.Net.Commands.Preconditions;
+using Xunit;
+
+namespace TaylorBot.Net.Commands.Types.Tests
+{
+    public class CommandExecutedHandlerTests
+    {
+        private readonly ILogger<CommandExecutedHandler> _logger = A.Fake<ILogger<CommandExecutedHandler>>(o => o.Strict());
+        private readonly IOngoingCommandRepository _ongoingCommandRepository = A.Fake<IOngoingCommandRepository>(o => o.Strict());
+        private readonly ICommandUsageRepository _commandUsageRepository = A.Fake<ICommandUsageRepository>(o => o.Strict());
+
+        private readonly CommandExecutedHandler _commandExecutedHandler;
+
+        public CommandExecutedHandlerTests()
+        {
+            _commandExecutedHandler = new CommandExecutedHandler(
+                _logger, _ongoingCommandRepository, _commandUsageRepository
+            );
+        }
+
+        [Fact]
+        public async Task OnCommandExecutedAsync_WhenUnknownCommand_ThenNoLog()
+        {
+            var commandContext = A.Fake<ITaylorBotCommandContext>(o => o.Strict());
+            A.CallTo(() => commandContext.OnGoingCommandAddedToPool).Returns(null);
+            var result = A.Fake<IResult>(o => o.Strict());
+            A.CallTo(() => result.Error).Returns(CommandError.UnknownCommand);
+
+            await _commandExecutedHandler.OnCommandExecutedAsync(Optional.Create<CommandInfo>(), commandContext, result);
+
+            A.CallTo(_logger).Where(call => call.Method.Name == nameof(ILogger.Log)).MustNotHaveHappened();
+        }
+    }
+}
