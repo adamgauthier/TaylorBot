4c80339541667aea42d0c7cee179f407cf8249a9
Adam Gauthier <adamgauthier12@gmail.com>
2020-12-25 16:13:45 -0500

    bump PostNotifier to .NET 5

--- src/TaylorBot.Net/Program.PostNotifier/src/TaylorBot.Net.InstagramNotifier.Infrastructure/InstagramRestClient.cs
+++ src/TaylorBot.Net/Program.PostNotifier/src/TaylorBot.Net.InstagramNotifier.Infrastructure/InstagramRestClient.cs
@@ -31,15 +31,15 @@ namespace TaylorBot.Net.InstagramNotifier.Infrastructure
             var captionEdges = item.GetProperty("edge_media_to_caption").GetProperty("edges").EnumerateArray().ToList();
 
             return new InstagramPost(
-                shortCode: item.GetProperty("shortcode").GetString(),
+                shortCode: item.GetProperty("shortcode").GetString()!,
                 takenAt: DateTimeOffset.FromUnixTimeSeconds(item.GetProperty("taken_at_timestamp").GetInt64()),
                 likesCount: item.GetProperty("edge_liked_by").GetProperty("count").GetUInt64(),
                 commentsCount: item.GetProperty("edge_media_to_comment").GetProperty("count").GetUInt64(),
-                thumbnailSrc: item.GetProperty("thumbnail_src").GetString(),
-                caption: captionEdges.Any() ? captionEdges.First().GetProperty("node").GetProperty("text").GetString() : null,
-                authorFullName: user.GetProperty("full_name").GetString(),
-                authorUsername: user.GetProperty("username").GetString(),
-                authorProfilePicUrl: user.GetProperty("profile_pic_url").GetString()
+                thumbnailSrc: item.GetProperty("thumbnail_src").GetString()!,
+                caption: captionEdges.Any() ? captionEdges.First().GetProperty("node").GetProperty("text").GetString()! : null,
+                authorFullName: user.GetProperty("full_name").GetString()!,
+                authorUsername: user.GetProperty("username").GetString()!,
+                authorProfilePicUrl: user.GetProperty("profile_pic_url").GetString()!
             );
         }
     }
