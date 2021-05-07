60cafe1fb78fbdc88b58e7f80bc98cb848dd5af5
Adam Gauthier <adamgauthier12@gmail.com>
2021-05-07 19:19:02 -0400

    respect role hierarchy in /kick

--- src/TaylorBot.Net/Program.Commands.Discord/src/TaylorBot.Net.Commands.Discord.Program/Modules/Mod/Commands/KickSlashCommand.cs
+++ src/TaylorBot.Net/Program.Commands.Discord/src/TaylorBot.Net.Commands.Discord.Program/Modules/Mod/Commands/KickSlashCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Humanizer;
+using System.Linq;
 using System.Threading.Tasks;
 using TaylorBot.Net.Commands.Discord.Program.Modules.Mod.Domain;
 using TaylorBot.Net.Commands.Events;
@@ -32,24 +33,46 @@ namespace TaylorBot.Net.Commands.Discord.Program.Modules.DiscordInfo.Commands
                 new(Name),
                 async () =>
                 {
+                    var author = (IGuildUser)context.User;
                     var member = options.member.Member;
 
-                    await member.KickAsync($"{context.User.FormatLog()} used /kick{(!string.IsNullOrEmpty(options.reason.Value) ? $": {options.reason.Value}" : " (No reason specified)")}".Truncate(MaxAuditLogReasonSize));
+                    var embed = new EmbedBuilder();
 
-                    await _modChannelLogger.TrySendModLogAsync(context.Guild!, context.User, member, logEmbed =>
+                    if (member.Guild.OwnerId == member.Id)
                     {
-                        if (!string.IsNullOrEmpty(options.reason.Value))
-                            logEmbed.AddField("Reason", options.reason.Value);
+                        embed
+                            .WithColor(TaylorBotColors.ErrorColor)
+                            .WithDescription($"You can't kick {member.FormatTagAndMention()} because they're the server owner!");
+                    }
+                    else if (author.Guild.OwnerId == author.Id || GetHighestRole(member).Position < GetHighestRole(author).Position)
+                    {
+                        await member.KickAsync($"{context.User.FormatLog()} used /kick{(!string.IsNullOrEmpty(options.reason.Value) ? $": {options.reason.Value}" : " (No reason specified)")}".Truncate(MaxAuditLogReasonSize));
 
-                        return logEmbed
-                            .WithColor(new(222, 184, 135))
-                            .WithFooter("User kicked");
-                    });
+                        await _modChannelLogger.TrySendModLogAsync(context.Guild!, context.User, member, logEmbed =>
+                        {
+                            if (!string.IsNullOrEmpty(options.reason.Value))
+                                logEmbed.AddField("Reason", options.reason.Value);
 
-                    return new EmbedResult(new EmbedBuilder()
-                        .WithColor(TaylorBotColors.SuccessColor)
-                        .WithDescription($"👢 Kicked {member.FormatTagAndMention()}")
-                    .Build());
+                            return logEmbed
+                                .WithColor(new(222, 184, 135))
+                                .WithFooter("User kicked");
+                        });
+
+                        embed
+                            .WithColor(TaylorBotColors.SuccessColor)
+                            .WithDescription($"👢 Kicked {member.FormatTagAndMention()}");
+                    }
+                    else
+                    {
+                        embed
+                            .WithColor(TaylorBotColors.ErrorColor)
+                            .WithDescription(string.Join('\n', new[] {
+                                $"You can't kick {member.FormatTagAndMention()} because their highest role is equal to or higher than yours in the roles list.",
+                                $"The order of roles in server settings is important, you can only kick someone whose role is lower than yours."
+                            }));
+                    }
+
+                    return new EmbedResult(embed.Build());
                 },
                 Preconditions: new ICommandPrecondition[] {
                     new InGuildPrecondition(),
@@ -57,6 +80,11 @@ namespace TaylorBot.Net.Commands.Discord.Program.Modules.DiscordInfo.Commands
                     new TaylorBotHasPermissionPrecondition(GuildPermission.KickMembers)
                 }
             ));
+        }
+
+        private static IRole GetHighestRole(IGuildUser member)
+        {
+            return member.Guild.Roles.Where(r => member.RoleIds.Contains(r.Id)).OrderByDescending(r => r.Position).First();
         }
     }
 }
