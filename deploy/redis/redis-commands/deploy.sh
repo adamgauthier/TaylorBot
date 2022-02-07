a094451297fb0eaf845840792cea12d2df44b47c
Adam Gauthier <adamgauthier12@gmail.com>
2022-02-06 17:44:34 -0800

    add data mount to redis deployment

--- deploy/redis/redis-commands/deploy.sh
+++ deploy/redis/redis-commands/deploy.sh
@@ -6,7 +6,19 @@ set -o nounset
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 network_name=$(cat "${__dir}/../../docker-network.name")
-name=$(cat "${__dir}/redis-commands.name")
+container_name=$(cat "${__dir}/redis-commands.name")
 password=$(cat "${__dir}/redis-commands.pass")
 
-docker container run -d --name $name --network ${network_name} redis redis-server --requirepass $password
+curr_date=`date +%Y.%m.%d-%H.%M.%S`
+container_path=~/${container_name}/${curr_date}-container
+
+data_path=${container_path}/data
+
+mkdir -p ${data_path}
+
+docker container run \
+    --detach \
+    --name ${container_name} \
+    --network ${network_name} \
+    --mount type=bind,source=${data_path},destination=/data \
+    redis redis-server --requirepass $password --save 3600 1 --loglevel warning
