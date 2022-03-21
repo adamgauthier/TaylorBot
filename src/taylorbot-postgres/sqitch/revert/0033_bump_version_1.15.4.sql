f761bc6cf058614412430014d4cc09179db6b40d
Adam Gauthier <adamgauthier12@gmail.com>
2022-03-20 23:08:21 -0700

    bump version to 1.15.4

---
+++ src/taylorbot-postgres/sqitch/revert/0033_bump_version_1.15.4.sql
@@ -0,0 +1,7 @@
+-- Revert taylorbot-postgres:0033_bump_version_1.15.4 from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ src/taylorbot-postgres/sqitch/verify/0033_bump_version_1.15.4.sql
@@ -0,0 +1,7 @@
+-- Verify taylorbot-postgres:0033_bump_version_1.15.4 on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
