f33b787f2e26f8703bb5659d17a0ab4d44040e7c
Adam Gauthier <adamgauthier12@gmail.com>
2024-05-25 15:06:29 -0700

    deploy taylorbot-redis to Azure Container Apps

--- src/taylorbot-redis/Deploy-Redis.ps1
+++ src/taylorbot-redis/Deploy-Redis.ps1
@@ -9,25 +9,44 @@ $ErrorActionPreference = "Stop"
 $config = Get-Content -Path "$PSScriptRoot/redis.$Environment.json" -Raw | ConvertFrom-Json
 
 if ($Environment -eq "Azure") {
-    Write-Output "Creating Azure Container Instance resource"
+    if ([string]::IsNullOrWhiteSpace($config.AzureContainerAppEnvName)) {
+        Write-Output "Creating Azure Container Instance resource"
 
-    $created = az container create `
-        --resource-group $config.AzureResourceGroupName `
-        --name $config.AzureResourceName `
-        --image redis `
-        --command-line "redis-server --requirepass $($config.RedisPassword) --save 3600 1 --loglevel warning" `
-        --ports 6379 `
-        --restart-policy Always `
-        --os-type Linux `
-        --ip-address Public `
-        --cpu 1 `
-        --memory 1 `
-        --output json
+        $created = az container create `
+            --resource-group $config.AzureResourceGroupName `
+            --name $config.AzureResourceName `
+            --image redis:latest `
+            --command-line "redis-server --requirepass $($config.RedisPassword)" `
+            --ports 6379 `
+            --restart-policy Always `
+            --os-type Linux `
+            --ip-address Public `
+            --cpu 1 `
+            --memory 1 `
+            --output json
 
-    $createdParsed = $created | ConvertFrom-Json
-    $redisHost = $createdParsed.ipAddress.ip
+        $createdParsed = $created | ConvertFrom-Json
+        $redisHost = $createdParsed.ipAddress.ip
 
-    Write-Output "Created resource with host $redisHost"
+        Write-Output "Created resource with host $redisHost"
+    }
+    else {
+        Write-Output "Creating Azure Container App resource"
+
+        az containerapp create `
+            --name $config.AzureResourceName `
+            --resource-group $config.AzureResourceGroupName `
+            --environment $config.AzureContainerAppEnvName `
+            --image redis:latest `
+            --cpu $config.ContainerCpu `
+            --memory $config.ContainerMemory `
+            --target-port 6379 `
+            --ingress internal `
+            --transport tcp `
+            --min-replicas 1 `
+            --max-replicas 1 `
+            --args "--requirepass $($config.RedisPassword)"
+    }
 }
 else {
     Write-Output "Creating local container"
@@ -52,5 +71,5 @@ else {
         --network $networkName `
         --mount "type=bind,source=$dataPath,destination=/data" `
         --publish "$($config.PublishedPort):6379" `
-        redis redis-server --requirepass $config.RedisPassword --save 3600 1 --loglevel warning
+        redis:latest redis-server --requirepass $config.RedisPassword
 }
