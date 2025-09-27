6b2043ab748780c0814427d58e555c43bc8b0d0a
Adam Gauthier <adamgauthier12@gmail.com>
2025-09-26 22:29:49 -0700

    use Places API (new) 90% of the time

--- src/TaylorBot.Net/Program.Commands.Discord/src/TaylorBot.Net.Commands.Discord.Program/DiscordCommandsProgram.cs
+++ src/TaylorBot.Net/Program.Commands.Discord/src/TaylorBot.Net.Commands.Discord.Program/DiscordCommandsProgram.cs
@@ -322,7 +322,7 @@ public static class DiscordCommandsProgram
             .AddTransient<ILocationClient>(provider =>
             {
                 var random = provider.GetRequiredService<IPseudoRandom>();
-                return random.GetInt32(0, 100) < 80
+                return random.GetInt32(0, 100) < 90
                     ? provider.GetRequiredService<GooglePlacesNewClient>()
                     : provider.GetRequiredService<GooglePlacesClient>();
             })
