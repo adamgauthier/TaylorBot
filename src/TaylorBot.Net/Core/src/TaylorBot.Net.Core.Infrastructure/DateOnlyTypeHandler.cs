0c19d157957211fed1c3bf66ec7d2d503b2722fd
Adam Gauthier <adamgauthier12@gmail.com>
2025-12-18 13:23:52 -0800

    fix DateOnly parsing with Npgsql 10 (#10)

--- src/TaylorBot.Net/Core/src/TaylorBot.Net.Core.Infrastructure/DateOnlyTypeHandler.cs
+++ src/TaylorBot.Net/Core/src/TaylorBot.Net.Core.Infrastructure/DateOnlyTypeHandler.cs
@@ -3,9 +3,10 @@ using System.Data;
 
 namespace TaylorBot.Net.Core.Infrastructure;
 
+// Dapper doesn't have built-in support for DateOnly
 public class DateOnlyTypeHandler : SqlMapper.TypeHandler<DateOnly>
 {
-    public override DateOnly Parse(object value) => DateOnly.FromDateTime((DateTime)value);
+    public override DateOnly Parse(object value) => value is DateOnly d ? d : DateOnly.FromDateTime((DateTime)value);
 
     public override void SetValue(IDbDataParameter parameter, DateOnly value)
     {
